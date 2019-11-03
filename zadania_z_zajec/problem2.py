7940ce0fac47f40e4176a9fe5318d7157f7b6e93
Kinia201201 <kingawielgus.kw@gmail.com>
2019-11-03 13:30:19 +0100

    vol2

--- zadania_z_zajec/problem2.py
+++ zadania_z_zajec/problem2.py
@@ -11,24 +11,44 @@ Created on Tue Oct 22 22:10:04 2019
 
 import matplotlib.pyplot as plt
 import numpy as np
+def funcX(value):
+    return value - (xc - value)
+def funcY(value):
+    return value - (yc - value)
+
 xc = 1
 yc = 2
-x = [3, 1, -2] # - xc
-y = [1, 2, 3] # -yc
+x = [-2, 3, 1, -2, 6] # 0 i 4 dodatkowe
+y = [1, 1, 2, 3, 4] 
 
-x = [2, 0, -1]
-y = [0, 1, 2]
+x = [i - xc for i in x]
+y = [i - yc for i in y]
 
-X, Y = np.meshgrid(x, y)
-u = X/np.sqrt(X ** 2 + Y ** 2)
-v = Y/np.sqrt(X ** 2 + Y ** 2)
+X = np.array(x)
+Y = np.array(y)
+#Y = [[-1, 0 , 1]]
+#X, Y = np.meshgrid(x, y)
+#X, Y = np.array(x, y)
+
+#funcX(X[j])
+#(Y[j] - (1 - Y[j]))
+j = 0
+for i in X:
+    u[j] = -X[j]/np.sqrt(funcX(X[j]) ** 2 + funcY(Y[j]) ** 2)
+    v[j] = -Y[j]/np.sqrt(funcX(X[j]) ** 2 + funcY(Y[j]) ** 2)
+    j = j + 1
+    
+#u = -X/np.sqrt(X ** 2 + Y ** 2)
+#v = -Y/np.sqrt(X ** 2 + Y ** 2)
+
+len = np.sqrt(u[0] ** 2 + v[0] ** 2)
 
 fig, ax = plt.subplots(figsize=(8,8))
-ax.quiver(X + xc,Y + yc,u,v)
+ax.quiver(X + xc,Y + yc ,u , v)
 
 
 
-ax.axis([-2, 5, -1, 6])
+ax.axis([-3, 7, 0, 5])
 ax.set_aspect('equal')
 
 plt.show()
